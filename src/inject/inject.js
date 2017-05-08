b448affa3c21df056209335b20b93583d3fe46d4
UrbanGeek <gupta.amlan@gmail.com>
2017-05-08 12:31:11 +0530

    basic framework

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,4 +1,6 @@
 chrome.extension.sendMessage({}, function(response) {
+    
+    
 	var readyStateCheckInterval = setInterval(function() {
 	if (document.readyState === "complete") {
 		clearInterval(readyStateCheckInterval);
