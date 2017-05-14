4d9c76df25b5868aea94cda42903cb5d6907a8d0
UrbanGeek <gupta.amlan@gmail.com>
2017-05-14 14:25:47 +0530

    integration with final design

--- src/inject/popover.js
+++ src/inject/popover.js
@@ -1,23 +1,26 @@
 function setDOMInfo(productDetails) {
-
-    /*
-    document.getElementById('asin').textContent   = productDetails.asin;
+    
     document.getElementById('itemName').textContent  = productDetails.itemName;
-    document.getElementById('itemPrice').textContent = productDetails.itemPrice;
-    document.getElementById('amazonFees').textContent = productDetails.amazonFees;
-    document.getElementById('fulfillCost').textContent = productDetails.fulfillCost;
-    document.getElementById('netProfit').textContent = productDetails.netProfit;
-*/
+    document.getElementById('asin').textContent   = productDetails.asin;    
+    document.getElementById('salePrice').textContent = productDetails.saleprice;    
+    document.getElementById('prodDimensions').textContent = Number(productDetails.itemdimension.Length)/100 + " X "+
+                                                            Number(productDetails.itemdimension.Width)/100 + " X "+
+                                                            Number(productDetails.itemdimension.Height)/100;    
+    document.getElementById('unitWeight').textContent = productDetails.saleprice;   
+    document.getElementById('amazonFees').textContent = productDetails.amazonFees || 0;
+    document.getElementById('fulfillCost').textContent = productDetails.fulfillCost || 0;
+    document.getElementById('revenue').textContent = productDetails.saleprice - productDetails.amazonFees - productDetails.fulfillCost;
+    
+    calculateWithProductCost(0.00);
+    
     document.getElementById('loader').style.display = "none"; 
     document.getElementById('details').style.display = "block";    
-    
 }
 
 
 function showError(msg){
     
-    document.getElementById('show-error-msg').textContent = msg;
-    
+    document.getElementById('show-error-msg').textContent = msg;    
     document.getElementById('loader').style.display = 'none'; 
     document.getElementById('show-error-msg').style.display = 'block';
     
@@ -26,65 +29,75 @@ function showError(msg){
 
 window.addEventListener('DOMContentLoaded', function () {
 
-  chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {        
-      
+    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {        
+
       var hostname = (new URL(tabs[0].url)).hostname;
-      
-      if (hostname === 'www.amazon.com'){
-          
-          chrome.tabs.sendMessage(tabs[0].id, {from: 'popup', subject: 'DOMInfo'}, function (response) {
-              
+      if (hostname === 'www.amazon.com'){          
+          chrome.tabs.sendMessage(tabs[0].id, {from: 'popup', subject: 'DOMInfo'}, function (response) {              
               if (!response) {
                   showError("Something went wrong! Please reload the page!");                  
               } else if (response.error) {
                   showError(response.error);
               } else {                  
                   setDOMInfo(response.productDetails);
-              }              
-              
+              }  
           });    
-          
+
       } else {
-          showError('Please visit www.amazon.com for using this extension.');
-          
+          showError('Please visit www.amazon.com for using this extension.');          
       }      
-      
-  });
+
+    });
     
     
     
     
-    
-    
- document.getElementById("productCostInput").addEventListener("keypress", function(){
-    
-    var productCost = Number(document.getElementById("productCostInput").value);
-    
-    if(!Number.isNaN(productCost)){
+    document.getElementById("productCostInput").addEventListener("keyup", function(){
+
+        var productCost = Number(document.getElementById("productCostInput").value);
+
+        if(!Number.isNaN(productCost)){        
+            calculateWithProductCost(productCost);
+        } else {       
+            document.getElementById("productCostInput").value = 0.00;
+            calculateWithProductCost(productCost);
+        }
+
+    });   
         
-        calculateWithProductCost(productCost);
-    }
-    
-});   
-    
-    
-    
-    
 });
 
 
 
 
 function calculateWithProductCost(productCost){
-    var revenue = Number(document.getElementById("revenue").textContent);
-    //var netProfit = Number(document.getElementById("netProfit"));
-    //var netMargin = Number(document.getElementById("netMargin"));
     
-    var newNetProfit = revenue - productCost;
     
-    document.getElementById("netProfit").textContent = netProfit;
+    var salePrice = Number(document.getElementById("salePrice").textContent);
+    var revenue = Number(document.getElementById("revenue").textContent); 
+    var newNetProfit = (revenue - productCost).toFixed(2);
+    var newNetMargin = (newNetProfit/salePrice*100).toFixed(2);
+    
+    var netProfitElement = document.getElementById("netProfit");
+    var netMarginElement = document.getElementById("netMargin");
+    
+    netProfitElement.textContent = newNetProfit;    
+    netMarginElement.textContent = newNetMargin;
+    
+    if(newNetProfit<0){        
+        classToElement(document.getElementById("net-section"),"loss","profit");  
+        
+    } else {
+        classToElement(document.getElementById("net-section"),"profit","loss");
+    }    
 
-    
+}
 
+
+function classToElement(el,toBeAdded,toBeRemoved){
+    
+    el.classList.add(toBeAdded);
+    el.classList.remove(toBeRemoved);
+    
 }
 
